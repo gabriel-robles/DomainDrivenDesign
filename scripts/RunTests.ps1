a704d6ed25d4daa4aa80ae660647849232572914
Gabriel <gabriel.lucas.asr@gmail.com>
2023-08-07 22:36:25 -0300

    refactor: Ajustando workflow para deploy da doc

--- scripts/RunTests.ps1
+++ scripts/RunTests.ps1
@@ -1,5 +1,5 @@
-coverlet tests/App.Api.UnitTest/bin/Debug/net6.0/App.Api.UnitTest.dll --target "dotnet" --targetargs "test /tests/App.Api.UnitTest --no-build"
-coverlet tests/App.Application.UnitTest/bin/Debug/net6.0/App.Application.UnitTest.dll --target "dotnet" --targetargs "test /tests/App.Application.UnitTest --no-build" --merge-with "coverage.json"
-coverlet tests/App.Infrastructure.UnitTest/bin/Debug/net6.0/App.Infrastructure.UnitTest.dll --target "dotnet" --targetargs "test /tests/App.Infrastructure.UnitTest --no-build" --merge-with "coverage.json"
-coverlet tests/App.Domain.UnitTest/bin/Debug/net6.0/App.Domain.UnitTest.dll --target "dotnet" --targetargs "test /tests/App.Domain.UnitTest --no-build" --merge-with "coverage.json" --format cobertura
+coverlet tests/App.Api.UnitTest/bin/Debug/net6.0/App.Api.UnitTest.dll --target "dotnet" --targetargs "test tests/App.Api.UnitTest --no-build"
+coverlet tests/App.Application.UnitTest/bin/Debug/net6.0/App.Application.UnitTest.dll --target "dotnet" --targetargs "test tests/App.Application.UnitTest --no-build" --merge-with "coverage.json"
+coverlet tests/App.Infrastructure.UnitTest/bin/Debug/net6.0/App.Infrastructure.UnitTest.dll --target "dotnet" --targetargs "test tests/App.Infrastructure.UnitTest --no-build" --merge-with "coverage.json"
+coverlet tests/App.Domain.UnitTest/bin/Debug/net6.0/App.Domain.UnitTest.dll --target "dotnet" --targetargs "test tests/App.Domain.UnitTest --no-build" --merge-with "coverage.json" --format cobertura
 reportgenerator -reports:coverage.cobertura.xml -targetdir:docs/_site/coveragereport -reportTypes:html
