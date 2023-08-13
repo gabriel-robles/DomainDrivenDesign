dd4e7f11606d12da245e09a8ad47cdf46230dc78
Gabriel <gabriel.lucas.asr@gmail.com>
2023-08-13 16:32:20 -0300

    refactor: adicionando code coverage no workflow de SAST

--- src/App.Api/DependencyInjection.cs
+++ src/App.Api/DependencyInjection.cs
@@ -1,15 +1,18 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace App.Api;
 
 /// <summary>
-///     Injeta as dependências da camada de apresentação da aplicação.
+/// Injeta as dependências da camada de apresentação da aplicação.
 /// </summary>
+[ExcludeFromCodeCoverage]
 public static class DependencyInjection
 {
     /// <summary>
-    ///     Adiciona os serviços no <see cref="IServiceCollection"/> designado.
+    /// Adiciona os serviços no <see cref="IServiceCollection"/> designado.
     /// </summary>
     /// <param name="services">sem os serviços.</param>
-    /// <returns>com os serviços.</returns>
+    /// <returns></returns>
     public static IServiceCollection AddPresentation(this IServiceCollection services)
     {
         services.AddControllers();
